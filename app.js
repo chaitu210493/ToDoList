4e2f9002341ae3cfc314e5a36096e246073b7cca
chaitu210493 <35973802+chaitu210493@users.noreply.github.com>
2020-07-05 15:56:02 +0530

    Update app.js
    Updated Mongoose connect with Generice values

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-chaitanya:test123@cluster0.itutc.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.itutc.mongodb.net/<dbName>", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
